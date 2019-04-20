5aafad5985c93f86b028c823e4a88c2e8bf4aa1c
Kiran Yedidi <kiran.yedidi@sharpspring.com>
2019-04-20 01:51:04 -0400

    merge conflicts

--- index.js
+++ index.js
@@ -1,40 +1,12 @@
-<<<<<<< Updated upstream
-const { ApolloServer } = require('apollo-server');
-
-const typeDefs = `
-
-  enum PhotoCategory {
-    SELFIE
-    PORTRAIT
-    ACTION
-    LANDSCAPE
-    GRAPHIC
-  }
-=======
 const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
 const expressPlayground = require('graphql-playground-middleware-express').default
 
 const { readFileSync } = require('fs');
->>>>>>> Stashed changes
 
 const typeDefs = readFileSync('./typedefs.graphql', 'UTF-8');
 const resolvers = require('./resolvers');
 
-<<<<<<< Updated upstream
-  type Photo {
-    id: ID!
-    url: String!
-    name: String!
-    description: String
-    category: PhotoCategory!
-  }
-
-  type Query {
-    totalPhotos: Int!
-    allPhotos(first: Int=1 start: Int=1): [Photo!]!
-  }
-=======
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
@@ -42,7 +14,6 @@ async function start() {
   const app = express();
   const MONGO_DB = process.env.DB_HOST;
   const client = await MongoClient.connect(MONGO_DB, { useNewUrlParser: true });
->>>>>>> Stashed changes
 
   const db = client.db();
   const context = { db };
@@ -50,46 +21,6 @@ async function start() {
 
   server.applyMiddleware({ app });
 
-<<<<<<< Updated upstream
-const resolvers = {
-  Query: {
-    totalPhotos: () => photos.length,
-    allPhotos: (parent, args) => {
-      const { first, start } = args;
-      var photosToReturn = [];
-      if (!(first || start)) {
-        photosToReturn = photos;
-      } else if (first && start && start + first <= photos.length + 1) {
-        for (i = start - 1; i < start - 1 + first; i++) {
-          photosToReturn.push(photos[i]);
-        }
-      }
-      return photosToReturn;
-    }
-  },
-  Mutation: {
-    postPhoto: (parent, args) => {
-      var newPhoto = {
-        id: photos.length + 1,
-        ...args.input
-      }
-      photos.push(newPhoto);
-      return newPhoto;
-    }
-  },
-  Photo: {
-    url: (parent) => `http://www.google.com/img/${parent.id}.jpg`,
-    id: (parent) => `Kiran - ${parent.id}`
-  }
-};
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-});
-
-server.listen().then(({ url }) => console.log(`GraphQL service running on ${url}`));
-=======
   app.get('/', (req, res) => res.end('welcome to my GraphQL learning platform'));
   app.get('/playground', expressPlayground({ endpoint: '/graphql' }));;
 
@@ -97,4 +28,3 @@ server.listen().then(({ url }) => console.log(`GraphQL service running on ${url}
 }
 
 start();
->>>>>>> Stashed changes
